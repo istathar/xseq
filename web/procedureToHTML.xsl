3666419aa98e4f7b4f7a39c8139e88f4d6d53e1e
Andrew Cowie <andrew@operationaldynamics.com>
2010-03-10 00:59:40 +1100

    Schema update
    Make titles explicit elements under section and step.  Update schema
    DTD file and XSLT stylesheet (what a pain with namespaces).
    
    Upgrade view.php convenience page to use PHP5 XSLTProcessor functions.
    Don't bother caching anymore; just re-run the transform each time.

--- web/procedureToHTML.xsl
+++ web/procedureToHTML.xsl
@@ -1,64 +1,65 @@
 <xsl:stylesheet version="1.0"
-xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+	xmlns:p="http://namespace.operationaldynamics.com/procedures/0.4"
+	exclude-result-prefixes="p">
 	<xsl:output method="html" indent="no" omit-xml-declaration="yes"/>
 
 	<xsl:variable name="num">7</xsl:variable>
 
 	<xsl:template match="/">
-		<H1><xsl:value-of select="/procedure/title"/></H1>
-		<xsl:if test="/procedure/overview"> 
-			<DL>
-			<DT><I>Overview</I></DT>
-			<DD><xsl:value-of select="/procedure/overview"/></DD>
-			</DL>
+		<h1><xsl:value-of select="/p:procedure/p:title"/></h1>
+		<xsl:if test="/p:procedure/p:overview"> 
+			<dl>
+			<dt><i>Overview</i></dt>
+			<dd><xsl:value-of select="/p:procedure/p:overview"/></dd>
+			</dl>
 		</xsl:if>
 		<xsl:apply-templates/>
 	</xsl:template>
 
-	<xsl:template match="/procedure">
-		<TABLE noborder="1" width="100%">
-		<TR> <TD bgcolor="#006699">Procedure</TD> </TR>
-		</TABLE>
-		<OL type="I">
-		<xsl:apply-templates select="section"/>
-		</OL>
+	<xsl:template match="/p:procedure">
+		<div style="background-color:#006699; color: white; padding: 5px;">
+		Procedure
+		</div>
+		<ol type="I">
+		<xsl:apply-templates select="p:section"/>
+		</ol>
 	</xsl:template>
 
-
-	<xsl:template match="section">
-		<LI>
-			<P><B><xsl:value-of select="@title"/></B></P>
-			<P><xsl:value-of select="precis"/></P>
-			<OL type="1">
-			<xsl:attribute name="start"><xsl:value-of select="count(preceding::step)+1"/></xsl:attribute>
-			<xsl:apply-templates select="step"/>
-			</OL>
-			<HR/>
-		</LI>
+	<xsl:template match="p:section">
+		<li>
+			<p><b><xsl:value-of select="p:title"/></b></p>
+			<p><xsl:value-of select="p:precis"/></p>
+			<ol type="1">
+			<xsl:attribute name="start"><xsl:value-of select="count(preceding::p:step)+1"/></xsl:attribute>
+			<xsl:apply-templates select="p:step"/>
+			</ol>
+			<hr/>
+		</li>
 	</xsl:template>
 
-	<xsl:template match="step">
-		<LI>
-		<xsl:value-of select="@title"/>
-			<UL>
-			<xsl:apply-templates select="name"/>
-			</UL>
-		</LI>
+	<xsl:template match="p:step">
+		<li>
+		<xsl:value-of select="p:title"/>
+			<ul>
+			<xsl:apply-templates select="p:name"/>
+			</ul>
+		</li>
 	</xsl:template>
 
-	<xsl:template match="name">
-		<LI>
+	<xsl:template match="p:name">
+		<li>
 		<xsl:value-of select="@who"/>
-			<OL type="a">
-			<xsl:apply-templates select="task"/>
-			</OL>
-		</LI>
+			<ol type="a">
+			<xsl:apply-templates select="p:task"/>
+			</ol>
+		</li>
 	</xsl:template>
 
-	<xsl:template match="task">
-		<LI>
+	<xsl:template match="p:task">
+		<li>
 		<xsl:value-of select="."/>
-		</LI>
+		</li>
 	</xsl:template>
 
 
--- web/view.php
+++ web/view.php
@@ -1,6 +1,4 @@
 <?php
-	#$debug = true;
-
 	# on the assumption that this script is living in web/, then
 	# the base directory of everything else will be the base of an 
 	# install tarball, ie .. . Most of all, this is so we can set the
@@ -9,10 +7,8 @@
 
 	chdir("..");
 	$basedir = getcwd();
-	$baseuri = "file://".$basedir."/";
 
 	$srcdir = $basedir."/doc/examples";
-	$outdir = $basedir."/web/cache";
 
 	$now = time();
 
@@ -21,140 +17,30 @@
 	$day  = date("D");
 
 	$srcfile = $srcdir."/simpleProcedure_v1_Example.xml";
-	$outfile = $outdir."/current.html";
 	$xslfile = $basedir."/web/procedureToHTML.xsl";
-
-	# Yes, this is cumbersome, but this level of granularity may help
-	# debug the very likely permissions problems that people have.
-
-	if (!file_exists($outdir)) {
-		if (!mkdir($outdir)) {
-			print "<P>Couldn't make output cache directory $outdir!";
-			exit;
-		}
-	}
-
-	$srcfile_mtime = @filemtime($srcfile);
-	$outfile_mtime = @filemtime($outfile);
-	$xslfile_mtime = @filemtime($xslfile);
-
-	if ($debug) {
-		print "<P><PRE>";
-
-		print "baseuri:  file://".$basedir."/<BR>";
-		print "srcfile:  ".$srcfile."<BR>";
-		print "xslfile:  ".$xslfile."<BR>";
-		print "outfile:  ".$outfile."<BR>";
-		print "<BR>";
-
-		print date("D M d G:i:s T Y", $now);
-		print ", time now<BR>";
-		if ($outfile_mtime == 0) {
-			print "----------------------------, cache .html file does not exist<BR>";
-		} else {
-			print date("D M d G:i:s T Y",$outfile_mtime);
-			print ", mtime of cache .html file<BR>";
-		}
-	
-		if ($srcfile_mtime == 0) {
-			print "----------------------------, source .xml file does not exist; abort coming up<BR>";
-		} else {
-			print date("D M d G:i:s T Y",$srcfile_mtime);
-			print ", mtime of source .xml file<BR>";
-		}
-
-		if ($xslfile_mtime == 0) {
-			print "----------------------------, XSL stylesheet (.xsl) missing or not found; abort coming up<BR>";
-		} else {
-			print date("D M d G:i:s T Y",$xslfile_mtime);
-			print ", mtime of .xsl stylesheet file<BR>";
-		}
-		print "</PRE></P>";
-	}
-
-	if ($srcfile_mtime == 0) {
-		print "<P>Source .xml file $srcfile does not exist. Aborting</P>";
-		exit;
-	}
-	if ($xslfile_mtime == 0) {
-		print "<P>Internally specified XSL stylesheet file $xslfile does not exist. Aborting</P>";
-		exit;
-	}
-
-	if ($srcfile_mtime > $outfile_mtime) {
-		if ($debug) {
-			print "<P>[Re]generating file!</P>\n";
-		}
-		$cache = false;
-	} else if ($xslfile_mtime > $outfile_mtime) {
-		if ($debug) {
-			print "<P>Regenerating file due to XSL stylesheet being newer!</P>\n";
-		}
-		$cache = false;
-	} else {
-		if ($debug) {
-			print "<P>Using existing file</P>\n";
-		}
-		$cache = true;
-	}
-
-	// special case: changed the XSLT file!
-	
 ?>
 <HTML>
 <HEAD>
-	<TITLE>HTML version of Procedure 
-<?
-	print "(";
-
-	if ($cache) {
-		print "Using cached file";
-	} else {
-		if ($outfile_mtime == 0) {
-			print "Generating cache file";
-		} else {
-			print "Regenerating cache file";
-		}
-	}
-	print ")";
-?>
-</TITLE>
+<TITLE>HTML version of Procedure</TITLE>
 </HEAD>
 <BODY>
 <?
 
-function xslt_trap_error($parser, $num, $level, $messages) {
-	global $xslt_trapped_errors;
+	$xslDoc = new DOMDocument();
+	$xslDoc->load($xslfile);
 
-	if (is_array($messages)) {
-		foreach ($messages as $key => $value) {
-			$data[$key] = $value;
-		}
-		$xslt_trapped_errors = sprintf("%s in %s on line %d", $data['msg'], $data['URI'], $data['line']);
+	$xmlDoc = new DOMDocument();
+	$xmlDoc->load($srcfile);
+
+	$proc = new XSLTProcessor();
+	$proc->importStylesheet($xslDoc);
+
+	$result = $proc->transformToXML($xmlDoc);
+
+	if ($result == FALSE) {
+		print "<P><B>Oops!</B> $xslt_trapped_errors (XSLT errno: ".libxml_get_last_error().")</P>";
 	} else {
-		$xslt_trapped_errors = $messages;
-	}
-}
-
-/* 
-	The main program....
-*/
-
-	global $xslt_trapped_errors;
-
-	if ($cache) {
-		include($outfile);
-	} else {
-		$xh = xsl_create();
-		xslt_set_error_handler($xh, xslt_trap_error);
-		xslt_set_base($xh, $baseuri);
-
-		if (@xslt_process($xh, $srcfile, $xslfile, $outfile)) {
-			include($outfile);
-		} else {
-			print "<P><B>Oops!</B> $xslt_trapped_errors (XSLT errno: ".xslt_errno($xh).")</P>";
-		}
-		xslt_free($xh);
+		print $result;
 	}
 ?>
 </BODY>
